7ca45948b9c87d820cb6479fed5213b69b0c7aba
Javid Hosseini <j.hhoseini@gmail.com>
2023-07-28 10:06:47 -0700

    add the form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Time Tracker
+        </h1>
+        <form>
+          <label htmlFor="activity">Activity:</label>
+          <input type="text" id="activity" name="activity" required />
+          <br />
+
+          <label htmlFor="longShortTerm">Long term/Short term:</label>
+          <select id="longShortTerm" name="longShortTerm" required>
+            <option value="">Select</option>
+            <option value="longTerm">Long Term</option>
+            <option value="shortTerm">Short Term</option>
+          </select>
+          <br />
+
+          <label htmlFor="growth">Growth:</label>
+          <select id="growth" name="growth" required>
+            <option value="">Select</option>
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+          <br />
+
+          <label htmlFor="risk">Risk:</label>
+          <select id="risk" name="risk" required>
+            <option value="">Select</option>
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+          <br />
+
+          <button type="submit">Submit</button>
+        </form>
       </header>
     </div>
   );
