e388c224aea72a3c2f802983b530ba875a3e77bc
Javid Hosseini <j.hhoseini@gmail.com>
2023-07-28 22:53:31 -0700

    DRY

--- src/ActivityFrom.js
+++ src/ActivityFrom.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { ButtonGroup, Button, TextField } from '@mui/material';
-import { longShortTermOptions, growthOptions, riskOptions } from './options';
 import { useCookies } from 'react-cookie';
+
+import { longShortTermOptions, growthOptions, riskOptions } from './options';
 
 function ActivityForm() {
   const [activity, setActivity] = useState('');
-  const [longShortTerm, setLongShortTerm] = useState(null);
-  const [growth, setGrowth] = useState(null);
-  const [risk, setRisk] = useState(null);
+  const [selectedOptions, setSelectedOptions] = useState({
+    longShortTerm: null,
+    growth: null,
+    risk: null,
+  });
   const [activityData, setActivityData] = useState([]); // State to store the array of activity data
   const [cookies, setCookies] = useCookies(['activityData']);
 
@@ -18,31 +21,27 @@ function ActivityForm() {
     }
   }, []); // The empty dependency array ensures this effect runs only once when the component mounts
 
-  const handleLongShortTermClick = (option) => {
-    setLongShortTerm(option);
+  const options = {
+    longShortTerm: longShortTermOptions,
+    growth: growthOptions,
+    risk: riskOptions,
   };
 
-  const handleGrowthClick = (option) => {
-    setGrowth(option);
-  };
-
-  const handleRiskClick = (option) => {
-    setRisk(option);
+  const handleOptionClick = (optionType, option) => {
+    setSelectedOptions((prevOptions) => ({ ...prevOptions, [optionType]: option }));
   };
 
   const handleSubmit = () => {
     // Check if any of the values are null (none)
-    if (activity === '' || longShortTerm === null || growth === null || risk === null) {
-        alert('Please fill in all the fields.');
-        return; // If any value is none, do not save the entry and return early
-        }
-      
+    if (activity === '' || Object.values(selectedOptions).some((option) => option === null)) {
+      alert('Please fill in all the fields.');
+      return; // If any value is none, do not save the entry and return early
+    }
+
     // Create a new activity entry object
     const newActivityEntry = {
       activity,
-      longShortTerm,
-      growth,
-      risk,
+      ...selectedOptions,
     };
 
     // Add the new activity entry to the activityData array
@@ -53,14 +52,15 @@ function ActivityForm() {
 
     // Clear the form inputs after submission
     setActivity('');
-    setLongShortTerm(null);
-    setGrowth(null);
-    setRisk(null);
+    setSelectedOptions({
+      longShortTerm: null,
+      growth: null,
+      risk: null,
+    });
 
     // Update the local state to reflect the changes
     setActivityData(updatedActivityData);
   };
-  console.log(activityData)
 
   return (
     <div>
@@ -78,53 +78,27 @@ function ActivityForm() {
       <br />
       <br />
 
-      <ButtonGroup>
-        {longShortTermOptions.map((option) => (
-          <Button
-            key={option.value}
-            variant={
-              longShortTerm && longShortTerm.value === option.value
-                ? 'contained'
-                : 'outlined'
-            }
-            onClick={() => handleLongShortTermClick(option)}
-          >
-            {option.label}
-          </Button>
-        ))}
-      </ButtonGroup>
-      <br />
-      <br />
-
-      <ButtonGroup>
-        {growthOptions.map((option) => (
-          <Button
-            key={option.value}
-            variant={
-              growth && growth.value === option.value ? 'contained' : 'outlined'
-            }
-            onClick={() => handleGrowthClick(option)}
-          >
-            {option.label}
-          </Button>
-        ))}
-      </ButtonGroup>
-      <br />
-      <br />
-
-      <ButtonGroup>
-        {riskOptions.map((option) => (
-          <Button
-            key={option.value}
-            variant={risk && risk.value === option.value ? 'contained' : 'outlined'}
-            onClick={() => handleRiskClick(option)}
-          >
-            {option.label}
-          </Button>
-        ))}
-      </ButtonGroup>
-      <br />
-      <br />
+      {Object.keys(options).map((optionType) => (
+        <React.Fragment key={optionType}>
+          <ButtonGroup >
+            {options[optionType].map((option) => (
+              <Button
+                key={option.value}
+                variant={
+                  selectedOptions[optionType] && selectedOptions[optionType].value === option.value
+                    ? 'contained'
+                    : 'outlined'
+                }
+                onClick={() => handleOptionClick(optionType, option)}
+              >
+                {option.label}
+              </Button>
+            ))}
+          </ButtonGroup>
+          <br />
+          <br />
+        </React.Fragment>
+      ))}
 
       <Button type="submit" variant="contained" color="primary" onClick={handleSubmit}>
         Submit
@@ -134,7 +108,7 @@ function ActivityForm() {
       <ul>
         {activityData.map((entry, index) => (
           <li key={index}>
-            Activity: {entry.activity}, Long/Short Term: {entry.longShortTerm.label}, 
+            Activity: {entry.activity}, Long/Short Term: {entry.longShortTerm.label},
             Growth: {entry.growth.label}, Risk: {entry.risk.label}
           </li>
         ))}
